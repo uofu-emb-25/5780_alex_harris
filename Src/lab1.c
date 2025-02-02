703c05f48965d1645290e7fcb0ef944605ee7e3e
Alex Harris <u0966308@utah.edu>
2025-02-02 14:27:09 -0700

    set all HAL functions to implemented functions

--- Src/lab1.c
+++ Src/lab1.c
@@ -5,7 +5,7 @@ int lab1_main(void) {
     HAL_Init();
     
     SystemClock_Config();
-    __HAL_RCC_GPIOC_CLK_ENABLE();
+    My_HAL_RCC_GPIOC_CLK_ENABLE();
     // Assert that GPIOC Clock is enabled
     assert(((RCC ->AHBENR & RCC_AHBENR_GPIOCEN_Msk) >> RCC_AHBENR_GPIOCEN_Pos) == 0x1);
 
@@ -17,27 +17,27 @@ int lab1_main(void) {
     
     My_HAL_GPIO_Init(GPIOC, &initStr);
 
-    // Assert that GPIOC pins 8 and 9 are set to push pull output mode
+    // Assert that GPIOC pins 6 and 7 are set to push pull output mode
     assert((GPIOC->OTYPER & GPIO_OTYPER_OT_6)  == 0);
     assert((GPIOC->OTYPER & GPIO_OTYPER_OT_7)  == 0);
 
 
-    // Assert that GPIOC pins 8 and 9 are set to low freq mode
+    // Assert that GPIOC pins 6 and 7 are set to low freq mode
     assert(((GPIOC ->OSPEEDR & GPIO_OSPEEDR_OSPEEDR6_Msk) >> GPIO_OSPEEDR_OSPEEDR6_Pos) == 0x0);
     assert(((GPIOC ->OSPEEDR & GPIO_OSPEEDR_OSPEEDR7_Msk) >> GPIO_OSPEEDR_OSPEEDR7_Pos) == 0x0);
 
-    // Assert that GPIOC pins 8 and 9 are set to no pull up or pull down activation
+    // Assert that GPIOC pins 6 and 7 are set to no pull up or pull down activation
     assert(((GPIOC ->PUPDR & GPIO_PUPDR_PUPDR6_Msk) >> GPIO_PUPDR_PUPDR6_Pos) == 0x0);
     assert(((GPIOC ->PUPDR & GPIO_PUPDR_PUPDR7_Msk) >> GPIO_PUPDR_PUPDR7_Pos) == 0x0);
 
 
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
+    My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
     // Assert that GPIOC 6 is set
     assert(HAL_GPIO_ReadPin(GPIOC,GPIO_PIN_6) == GPIO_PIN_SET);
 
     while (1) {
         HAL_Delay(200);
-        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_7);
+        My_HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_6 | GPIO_PIN_7);
         //Assert that the GPIOC 6 and GPIO 7 are not in the same state.
         assert(HAL_GPIO_ReadPin(GPIOC,GPIO_PIN_6) != HAL_GPIO_ReadPin(GPIOC,GPIO_PIN_7));
     }
