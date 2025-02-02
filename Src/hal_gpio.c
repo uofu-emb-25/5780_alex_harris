9b8d9b7370bab1ee3bba50338cfb30525c63cb1b
Alex Harris <u0966308@utah.edu>
2025-02-02 14:26:34 -0700

    add GPIOx toggle pin function

--- Src/hal_gpio.c
+++ Src/hal_gpio.c
@@ -35,7 +35,7 @@ GPIO_PinState HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
 {
     if(PinState = GPIO_PIN_SET){
-        GPIOx->BSRR = (uint32_t)GPIO_Pin;
+        GPIOx->BSRR == (uint32_t)GPIO_Pin;
     }
     else{
         GPIOx->BRR = (uint32_t)GPIO_Pin;
@@ -43,8 +43,12 @@ void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState 
 }
 
 
-/*
-void HAL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
+
+void My_HAL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
+    if(GPIOx->ODR & GPIO_Pin)
+        GPIOx->BSRR = GPIO_Pin << 16;
+    else{
+        GPIOx->BSRR = GPIO_Pin;
+    }
 }
-*/
