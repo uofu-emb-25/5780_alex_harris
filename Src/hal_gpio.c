50c51eb9e580bd6f6c4c1c380d91557f8c62a267
Alex Harris <u0966308@utah.edu>
2025-02-03 19:42:07 -0700

    implement pinstate using idr

--- Src/hal_gpio.c
+++ Src/hal_gpio.c
@@ -30,12 +30,10 @@ void HAL_GPIO_DeInit(GPIO_TypeDef  *GPIOx, uint32_t GPIO_Pin)
 }
 */
 
-/*
 GPIO_PinState HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
-    return -1;
+    return GPIOx->IDR & GPIO_Pin;
 }
-*/
 
 
 void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
