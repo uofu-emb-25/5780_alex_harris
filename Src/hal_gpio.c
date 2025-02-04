cea3d06c50bc68207caba7cabc54ede11d86c4b6
Alex Harris <u0966308@utah.edu>
2025-02-03 19:17:28 -0700

    fix togglepin and writepin methods

--- Src/hal_gpio.c
+++ Src/hal_gpio.c
@@ -34,8 +34,8 @@ GPIO_PinState HAL_GPIO_ReadPin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 
 void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState PinState)
 {
-    if(PinState = GPIO_PIN_SET){
-        GPIOx->BSRR == (uint32_t)GPIO_Pin;
+    if(PinState == GPIO_PIN_SET){
+        GPIOx->BSRR = (uint32_t)GPIO_Pin;
     }
     else{
         GPIOx->BRR = (uint32_t)GPIO_Pin;
@@ -46,9 +46,10 @@ void My_HAL_GPIO_WritePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin, GPIO_PinState 
 
 void My_HAL_GPIO_TogglePin(GPIO_TypeDef* GPIOx, uint16_t GPIO_Pin)
 {
-    if(GPIOx->ODR & GPIO_Pin)
+    if (GPIOx->ODR & GPIO_Pin){
         GPIOx->BSRR = GPIO_Pin << 16;
-    else{
-        GPIOx->BSRR = GPIO_Pin;
     }
+    else{
+    GPIOx->BSRR = GPIO_Pin;
+   }
 }
