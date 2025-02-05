6ba1028f8087103abb274f5a40c38b609024f8ac
Alex Harris <u0966308@utah.edu>
2025-02-05 08:57:40 -0700

    setup pa0

--- Src/lab2.c
+++ Src/lab2.c
@@ -5,19 +5,25 @@ int lab2_main(void){
     HAL_Init();
     SystemClock_Config();
     My_HAL_RCC_GPIOC_CLK_ENABLE(); // Enable the GPIOC clock in the RCC   
+    My_HAL_RCC_GPIOC_CLK_ENABLE(); // Enable the GPIOC clock in the RCC   
 
-    GPIO_InitTypeDef initStr = {GPIO_PIN_6 | GPIO_PIN_7, 
+    GPIO_InitTypeDef initStr = {GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9, 
                                 GPIO_MODE_OUTPUT_PP, 
                                 GPIO_SPEED_FREQ_LOW, 
                                 GPIO_NOPULL};
+    HAL_GPIO_Init(GPIOC, &initStr);
 
-    My_HAL_GPIO_Init(GPIOC, &initStr);
+    GPIO_InitTypeDef initStr = {GPIO_PIN_0, 
+                                GPIO_MODE_INPUT, 
+                                GPIO_SPEED_FREQ_LOW, 
+                                GPIO_PULLDOWN};
+    HAL_GPIO_Init(GPIOA, &initStr);
 
     My_HAL_GPIO_WritePin(GPIOC,GPIO_PIN_9, GPIO_PIN_SET);
 
 
     while(1){
-        My_HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_6);
+        HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_6);
         HAL_Delay(400);
     }
 }
