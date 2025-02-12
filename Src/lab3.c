5b6b0c5809932dfc891d24cfcc3775f1585ac1b3
Alex Harris <u0966308@utah.edu>
2025-02-12 09:02:58 -0700

    implement lab3 main

--- Src/lab3.c
+++ Src/lab3.c
@@ -1,6 +1,13 @@
 #include <stm32f0xx_hal.h>
 #include <assert.h>
 
+void TIM2_IRQn_IRQHandler(void){
+    HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_8);
+    HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_9);
+
+    TIM2->SR|= (1 << 0); 
+}
+
 int lab3_main(void){
     HAL_Init();
     SystemClock_Config();
@@ -11,4 +18,9 @@ int lab3_main(void){
         GPIO_SPEED_FREQ_LOW, 
         GPIO_NOPULL};
     HAL_GPIO_Init(GPIOC, &initStr);
+    My_HAL_GPIO_WritePin(GPIOC,GPIO_PIN_8,GPIO_PIN_SET);
+
+
+    NVIC_EnableIRQ(TIM2_IRQn);
+    NVIC_SetPriority(TIM2_IRQn,3);
 }
