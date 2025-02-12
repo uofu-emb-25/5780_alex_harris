35a87a4749f9643b48d2e16c706916714f2bf7f0
Alex Harris <u0966308@utah.edu>
2025-02-12 08:06:03 -0700

    add hal timer peripheral enable method

--- Inc/main.h
+++ Inc/main.h
@@ -5,3 +5,4 @@ void SystemClock_Config(void);
 void My_HAL_RCC_GPIOC_CLK_ENABLE(void);
 void My_HAL_EXTI_ENABLE(void);
 void My_HAL_SYSCFG_PA0_ENABLE(void);
+void My_HAL_TIMER_PERIPHERAL_ENABLE(void);
