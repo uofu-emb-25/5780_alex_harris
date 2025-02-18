cae4e1e8ab6205ec3c910ab7de71ebf6dff25b6c
Alex Harris <u0966308@utah.edu>
2025-02-18 13:43:17 -0700

    clean up hal timer

--- Src/hal_timer.c
+++ Src/hal_timer.c
@@ -7,24 +7,25 @@ void My_HAL_TIMER_PERIPHERAL_ENABLE(void)
     RCC->APB1ENR|= RCC_APB1ENR_TIM2EN;
     RCC->APB1ENR|= RCC_APB1ENR_TIM3EN;
 
-    TIM2->PSC = 8000;
+    TIM2->PSC = 7999;
     TIM2->ARR = 250;
-    TIM3->PSC = 25599;
-    TIM3->ARR = 250000;
+    TIM3->PSC = 99;
+    TIM3->ARR = 99;
 
     TIM2->DIER|= TIM_DIER_UIE;
     TIM2->CR1 |= TIM_CR1_CEN;
 
+    
+
+    TIM3->CCMR1 &= ~(TIM_CCMR1_CC1S | TIM_CCMR1_CC2S);
+    TIM3->CCMR1 |= (6 << TIM_CCMR1_OC1M_Pos);
+    TIM3->CCMR1 |= (6 << TIM_CCMR1_OC2M_Pos);
+    TIM3->CCER  |= TIM_CCER_CC1E | TIM_CCER_CC2E;
+
     TIM3->CR1 |= TIM_CR1_CEN;
 
-    TIM3->CCMR1 &= ~TIM_CCMR1_CC1S;
-    TIM3->CCMR1 &= ~TIM_CCMR1_CC2S;
-    TIM3->CCMR1 &= (7 << 4);    
-    TIM3->CCMR1 |= (1 << 13) | (1 << 14);
-    TIM3->CCMR1 &= ~(1 << 12);
-    TIM3->CCMR1 |= (1 << 11) | (1 << 3);
-    TIM3->CCER  |= (1 << 0)  | (1 << 4);
-    TIM3-> CCR1 = 50000;
-    TIM3-> CCR2 = 50000;
+    TIM3-> CCR1 = 20;
+    TIM3-> CCR2 = 50;
+
 }
 
