a70b6d34bdeed183ca287d38c1608d4d4b86143d
Alex Harris <u0966308@utah.edu>
2025-02-25 10:17:53 -0700

    add two character input

--- Src/lab4.c
+++ Src/lab4.c
@@ -1,10 +1,23 @@
 #include <stm32f0xx_hal.h>
 #include <assert.h>
 
-void transmit_message(char input){
-    while((USART1->ISR & USART_ISR_TXE) == 0){}
-    USART1->TDR = input;
+void transmit_message(char *input){
+    while(*input){
+        while((USART1->ISR & USART_ISR_TXE) == 0){}
+        USART1->TDR = *input++;
+    }
 }
+volatile char receive = 0;
+volatile int newData = 0;
+
+void USART1_IRQHandler(void){
+    if(USART1->ISR & USART_ISR_RXNE){
+        receive = (char)(USART1->RDR & 0xFF);
+        newData = 1;
+        transmit_message(receive);
+    }
+}
+
 int lab4_main(void){
     HAL_Init();
 
@@ -21,27 +34,65 @@ int lab4_main(void){
         GPIO_NOPULL};
 
     HAL_GPIO_Init(GPIOC, &initStr);
+    NVIC_EnableIRQ(USART1_IRQn);
+    NVIC_SetPriority(USART1_IRQn,0);
+    USART1->CR1 |= USART_CR1_RXNEIE;
 
     while(1){
-        if((USART1->ISR & USART_ISR_RXNE) == 1){
-            char val = (char)(USART1->RDR & 0xFF);
-            switch(val){
-                case ('r'):
-                    HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_6);
-                    break;
-                case ('b'):
-                    HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_7);
-                    break;
-                case ('o'):
-                    HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_8);
-                    break;
-                case ('g'):
-                    HAL_GPIO_TogglePin(GPIOC,GPIO_PIN_9);
-                    break;
-                default:
-                    USART1->TDR = 'e';
-                    break;
-            }
+        char led = 0;
+        char funct = 0;
+        uint16_t led_pin = 0;
+        char *led_color = "";
+        transmit_message("CMD? ");
+
+        while(newData == 0){}
+        led = receive;
+        newData = 0;
+
+        while(newData == 0){}
+        funct = receive;
+        newData = 0;
+           
+        switch (led) {
+            case 'r': 
+                led_pin = GPIO_PIN_6;
+                transmit_message("red ");
+                break;
+            case 'b': 
+                led_pin = GPIO_PIN_7;
+                transmit_message("blue ") ;
+                break;
+            case 'o': 
+                led_pin = GPIO_PIN_8;
+                transmit_message("orange ") ;
+                break;
+            case 'g': 
+                led_pin = GPIO_PIN_9;
+                transmit_message("green ") ;
+                break;
+            default:
+                transmit_message("Error\r\n");
+                break;
+        }
+        switch (funct) {
+            case '0':
+                My_HAL_GPIO_WritePin(GPIOC, led_pin, GPIO_PIN_RESET);
+                transmit_message(*led_color);
+                transmit_message(" off\r\n");
+                break;
+            case '1':
+                My_HAL_GPIO_WritePin(GPIOC, led_pin, GPIO_PIN_SET);
+                transmit_message(*led_color);
+                transmit_message(" on\r\n");
+                break;
+            case '2':
+                HAL_GPIO_TogglePin(GPIOC, led_pin);
+                transmit_message(*led_color);
+                transmit_message(" toggle\r\n");
+                break;
+            default:
+                transmit_message("Error\r\n");
+                break;
         }
     }
 }
