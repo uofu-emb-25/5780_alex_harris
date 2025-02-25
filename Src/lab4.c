4c4c0617470d13d2c93e42a6741c8d071b23ce1a
Alex Harris <u0966308@utah.edu>
2025-02-25 08:42:05 -0700

    formatting

--- Src/lab4.c
+++ Src/lab4.c
@@ -2,14 +2,16 @@
 #include <assert.h>
 
 void transmit_message(char input){
-    while(USART1->ISR & USART_ISR_TXE == 0){}
+    while((USART1->ISR & USART_ISR_TXE) == 0){}
     USART1->TDR = input;
 }
 int lab4_main(void){
     HAL_Init();
+
     My_HAL_RCC_GPIOA_CLK_ENABLE();
-    My_HAL_RCC_GPIOC_CLK_ENABLE(); // Enable the GPIOC clock in the RCC   
+    My_HAL_RCC_GPIOC_CLK_ENABLE(); 
     My_HAL_RCC_USART_CLK_ENABLE();
+
     My_HAL_USART_ALTERNATE_FUNCTION_ENABLE();
     My_HAL_USART_CONFIGURE_PARAMS();
 
@@ -17,10 +19,11 @@ int lab4_main(void){
         GPIO_MODE_OUTPUT_PP, 
         GPIO_SPEED_FREQ_LOW, 
         GPIO_NOPULL};
+
     HAL_GPIO_Init(GPIOC, &initStr);
 
     while(1){
-        transmit_message('hello');
+        transmit_message('h');
         HAL_Delay(1000);
     }
 }
