79d73d5ab76bef211d30197c77df98564234780b
Alex Harris <u0966308@utah.edu>
2025-02-25 08:01:14 -0700

    implement transmit message function and add led enables

--- Src/lab4.c
+++ Src/lab4.c
@@ -1,8 +1,20 @@
 #include <stm32f0xx_hal.h>
 #include <assert.h>
 
+void transmit_message(char input){
+    while(USART1->ISR & USART_ISR_TXE == 0){}
+    USART1->TDR = input;
+}
 int lab4_main(void){
     HAL_Init();
     My_HAL_RCC_GPIOA_CLK_ENABLE();
+    My_HAL_RCC_GPIOC_CLK_ENABLE(); // Enable the GPIOC clock in the RCC   
     My_HAL_USART_ALTERNATE_FUNCTION_ENABLE();
+    My_HAL_USART_CONFIGURE_PARAMS();
+
+    GPIO_InitTypeDef initStr = {GPIO_PIN_8 | GPIO_PIN_9, 
+        GPIO_MODE_OUTPUT_PP, 
+        GPIO_SPEED_FREQ_LOW, 
+        GPIO_NOPULL};
+    HAL_GPIO_Init(GPIOC, &initStr);
 }
