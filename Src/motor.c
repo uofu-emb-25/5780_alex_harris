8123cd3e990828ae18ee3684a3939e9b2a185950
Alex Harris <u0966308@utah.edu>
2025-04-07 10:58:37 -0600

    pi calculation config

--- Src/motor.c
+++ Src/motor.c
@@ -13,7 +13,7 @@ volatile int8_t adc_value = 0;      	// ADC measured motor current
 volatile int16_t error = 0;         	// Speed error signal
 volatile uint8_t Kp = 1;            	// Proportional gain
 volatile uint8_t Ki = 1;            	// Integral gain
-
+volatile int16_t target_speed = 0;
 // Sets up the entire motor drive system
 void motor_init(void) {
     pwm_init();
@@ -182,49 +182,27 @@ void PI_update(void) {
      *
      */
     
-    /// TODO: calculate error signal and write to "error" variable
+    target_speed = (target_rpm * 64) / 60;
+    error = target_speed - motor_speed;
     
-    /* Hint: Remember that your calculated motor speed may not be directly in RPM!
-     *       You will need to convert the target or encoder speeds to the same units.
-     *       I recommend converting to whatever units result in larger values, gives
-     *       more resolution.
-     */
-    
-    
-    /// TODO: Calculate integral portion of PI controller, write to "error_integral" variable
-    
-    /// TODO: Clamp the value of the integral to a limited positive range
-    
-    /* Hint: The value clamp is needed to prevent excessive "windup" in the integral.
-     *       You'll read more about this for the post-lab. The exact value is arbitrary
-     *       but affects the PI tuning.
-     *       Recommend that you clamp between 0 and 3200 (what is used in the lab solution)
-     */
-    
-    /// TODO: Calculate proportional portion, add integral and write to "output" variable
-    
-    int16_t output = 0; // Change this!
-    
-    /* Because the calculated values for the PI controller are significantly larger than 
-     * the allowable range for duty cycle, you'll need to divide the result down into 
-     * an appropriate range. (Maximum integral clamp / X = 100% duty cycle)
-     * 
-     * Hint: If you chose 3200 for the integral clamp you should divide by 32 (right shift by 5 bits), 
-     *       this will give you an output of 100 at maximum integral "windup".
-     *
-     * This division also turns the above calculations into pseudo fixed-point. This is because
-     * the lowest 5 bits act as if they were below the decimal point until the division where they
-     * were truncated off to result in an integer value. 
-     *
-     * Technically most of this is arbitrary, in a real system you would want to use a fixed-point
-     * math library. The main difference that these values make is the difference in the gain values
-     * required for tuning.
-     */
+    error_integral = error_integral + error;
 
-     /// TODO: Divide the output into the proper range for output adjustment
-     
-     /// TODO: Clamp the output value between 0 and 100 
-    
+    if(error_integral > 3200){
+        error_integral = 3200;
+    }
+    else if(error_integral < 0){
+        error_integral = 0;
+    }
+        
+    int16_t output = Kp * error + Ki * error_integral; // Change this!
+
+    output = output >> 5;
+    if(output > 100){
+        output = 100;
+    }
+    else if(output < 0){
+        output = 0;
+    }
     pwm_setDutyCycle(output);
     duty_cycle = output;            // For debug viewing
 
